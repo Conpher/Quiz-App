754f168174cbd5d2be384445ba47b65b4d944b58
Conpher <christopherconlan23@gmail.com>
2020-02-27 21:11:44 +0000

    Initial Files Added

---
+++ app/src/main/java/org/me/gcu/honoursprojectapplication/ResetPasswordActivity.java
@@ -0,0 +1,14 @@
+package org.me.gcu.honoursprojectapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
