2553a5af2442ea1815261c5d32f5deb91a3993f8
Conpher <christopherconlan23@gmail.com>
2020-03-01 18:49:02 +0000

    Register system has been reworked with login link functionality. Login page has been removed and will be added in next commit.

--- app/src/main/java/org/me/gcu/honoursprojectapplication/MainActivity.java
+++ app/src/main/java/org/me/gcu/honoursprojectapplication/MainActivity.java
@@ -2,13 +2,36 @@ package org.me.gcu.honoursprojectapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputMail, inputPwd;
+    private Button btnLogin;
+    private TextView txtViewSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputMail = (EditText) findViewById(R.id.email);
+        inputPwd = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.sign_up_btn);
+        txtViewSignUp = (TextView) findViewById(R.id.sign_up_txt);
+
+        //This on click method is called whenever the txtViewSignUp has been selected
+        txtViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/org/me/gcu/honoursprojectapplication/SignUpActivity.java
@@ -0,0 +1,106 @@
+package org.me.gcu.honoursprojectapplication;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+   private EditText inputMail, inputPwd;
+   private Button btnSignUp;
+   private TextView txtViewLogin;
+   private ProgressBar progressBar;
+   private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        inputMail = (EditText) findViewById(R.id.email);
+        inputPwd = (EditText) findViewById(R.id.password);
+        btnSignUp = (Button) findViewById(R.id.sign_up_btn);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        txtViewLogin = (TextView) findViewById(R.id.login_txt);
+
+        //Create an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+
+        //Set OnClickListener for when user selects "already has an account, login".
+        txtViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+            }
+        });
+
+        //set OnClickListener for when user hits "Sign-Up" button.
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = inputMail.getText().toString().trim();
+                String pwd = inputPwd.getText().toString().trim();
+
+                //If email is empty display error message and return.
+                if (mail.isEmpty()){
+                    inputMail.setError("E-Mail is required!");
+                    inputMail.requestFocus();
+                    return;
+                }
+
+                //Check if user has entered a valid email, if not display error message and return.
+                if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
+                    inputMail.setError("Please enter a valid email!");
+                    inputMail.requestFocus();
+                    return;
+                }
+
+                //If user has not entered a password, display error message and return.
+                if (pwd.isEmpty()){
+                    inputPwd.setError("Password is required!");
+                    inputPwd.requestFocus();
+                    return;
+                }
+
+                //Ensure user has met the minimum password length of 6. If not display error msg.
+                if (pwd.length()<6){
+                    inputPwd.setError("Minimum length of password should be 6!");
+                    inputPwd.requestFocus();
+                    return;
+                }
+
+                //Progress bar should become visible when user has clicked the "Sign-Up" button.
+                progressBar.setVisibility(View.VISIBLE);
+
+                //Create a new user method which will authenticate and confirm email and pwd details.
+                mAuth.createUserWithEmailAndPassword(mail, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //Hide visibility of progress bar once event has been completed.
+                        progressBar.setVisibility(View.GONE);
+                        //If account is created successfully display message to the user.
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Account Created Successfully",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
+
